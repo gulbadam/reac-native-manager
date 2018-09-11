cfad57ce1db8917b31a36c7425acc544ffde8296
yelena guliyeva <gulbadam@gulbadam.com>
2018-09-11 12:48:23 -0700

    firebase setup

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import firebase from 'firebase';
+import reducers from './src/reducers'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,10 +19,22 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
-store = createStore()
+store = createStore(reducers)
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentWillMount = () => {
+    const  config = {
+      apiKey: 'AIzaSyA-8TP2RT4pTWcA3N-sQdMl7d_VRRPIgOo',
+      authDomain: 'manager-83d7b.firebaseapp.com',
+      databaseURL: 'https://manager-83d7b.firebaseio.com',
+      projectId: 'manager-83d7b',
+      storageBucket: 'manager-83d7b.appspot.com',
+      messagingSenderId: '412567920038'
+    };
+    firebase.initializeApp(config);
+  };
+  
   render() {
     return (
       <Provider store={store}>
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import {conbineReducers, combineReducers} from 'redux';
+export default combineReducers({
+banana: ()=>[]
+});
