3d4f8cc2f2dcb91b6ae3401b4e1784a619d8bfc7
yelena guliyeva <gulbadam@gulbadam.com>
2018-09-25 18:59:23 -0700

    refactor actions

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
- import { Scene, Router } from 'react-native-router-flux';
+ import { Scene, Router, Actions } from 'react-native-router-flux';
  import LoginForm from './components/LoginForm';
- import EmployeeList from './components/EmployeeList'
+ import EmployeeList from './components/EmployeeList';
+ import EmployeeCreate from './components/EmployeeCreate'
 const RouterComponent =()=> {
     return (
 <Router>
@@ -11,8 +12,9 @@ const RouterComponent =()=> {
 </Scene>
 <Scene key='main'>
 <Scene key='employeeList' component={EmployeeList} title="Employees"
-rightTitle='Add' onRight={()=>{console.log('right!!!')}}
+rightTitle='Add' onRight={()=>{Actions.employeeCreate()}} initial
  />
+ <Scene key='employeeCreate'component={EmployeeCreate} title="Create Employee"/>
 
 </Scene>
 </Scene>
